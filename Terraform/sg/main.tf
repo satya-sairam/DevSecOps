6ef100e35aa35f79e7415ae26bf92060c56858b4
Satya <satyasairam144@gmail.com>
2024-02-12 23:46:44 +0530

    changed sg

--- Terraform/sg/main.tf
+++ Terraform/sg/main.tf
@@ -1,14 +1,38 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = "vpc-0dda9b38eacc6ef2d"
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Allow inbound traffic on port 80 and 443"
 
+  // Ingress rules - allow incoming traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // Allow traffic from any IPv4 address
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // Allow traffic from any IPv4 address
+  }
+
+  // Egress rules - allow outgoing traffic (optional)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" // Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"] // Allow traffic to any IPv4 address
+  }
+
+  // Tags (optional)
   tags = {
-    Name = "allow_tls"
+    Name = "example-sg"
+    Environment = "production"
   }
 }
 
 
 output "sg_id_sg_module" {
-  value = aws_security_group.allow_tls.id
+  value = aws_security_group.example.id
 }
