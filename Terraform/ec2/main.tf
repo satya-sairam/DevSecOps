86039a8acdab151fc67daef534e6de51d2111159
Satya <satyasairam144@gmail.com>
2024-02-12 23:35:55 +0530

    typo

--- Terraform/ec2/main.tf
+++ Terraform/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0e731c8a588258d0d"
   instance_type = "t2.micro"
-  security_groups = [var.sg_id_ec2]
+  vpc_security_group_ids = [var.sg_id_ec2]
 
     tags = {
     Name = "tf-example"
