12750ef07b1deae934ea34584ad205f2264df618
Satya <satyasairam144@gmail.com>
2024-02-13 16:58:30 +0530

    nat EIP

--- terraform/modules/networking/nat.tf
+++ terraform/modules/networking/nat.tf
@@ -1,7 +1,15 @@
 resource "aws_nat_gateway" "ngw" {
+    allocation_id       = aws_eip.my_eip.id
+
   subnet_id             = aws_subnet.pub_subnet.id
   tags = {
     Name                = "${var.env}-ngw"
   }
   depends_on            = [aws_internet_gateway.igw]
+}
+
+
+resource "aws_eip" "my_eip" {
+    depends_on = [ aws_internet_gateway.igw ]
+  
 }
