f8ba6e118353a00220e079f4a9cbb55cf8fe34ea
Satya <satyasairam144@gmail.com>
2024-02-13 19:41:06 +0530

    changed backend

--- terraform/modules/networking/provider.tf
+++ terraform/modules/networking/provider.tf
@@ -1,16 +1,7 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-      
-    }
-  }
-  backend "s3" {
-    
-  }
 
- 
+  backend "s3" {}
+
 }
 
 provider "aws" {
