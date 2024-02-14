041684a174fe238e79f5f32ef883f12a11d3f3a3
Satya <satyasairam144@gmail.com>
2024-02-14 16:58:24 +0530

    deleted depends on ec2

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_ec2" {
   availability_zone             = data.aws_availability_zones.available[0]
   key_name                      = "ec2-ssh"
   associate_public_ip_address   = true
-  depends_on = [aws ]
+
 
   tags = {
     Name = "${var.env}-instance"
--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -4,5 +4,3 @@ variable "pub_subnet_cidr" {}
 variable "pvt_subnet_cidr" {}  
 variable "region" {}
 variable "instance_type" {}
-variable "sg_id" {}
-variable "vpc_id" {}
