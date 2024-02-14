88b9ec902da997b4016c6f7a795aee93fcc2ef35
Satya <satyasairam144@gmail.com>
2024-02-14 17:00:45 +0530

    data source fix

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_ec2" {
   instance_type                 = var.instance_type
   vpc_security_group_ids        = [var.sg_id]
   subnet_id                     = var.pub_subnet_id
-  availability_zone             = data.aws_availability_zones.available[0]
+  availability_zone             = data.aws_availability_zones.available.names[0]
   key_name                      = "ec2-ssh"
   associate_public_ip_address   = true
 
