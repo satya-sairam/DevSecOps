e13ec0ffbb69398f1bee3b83b268f47aa0c1e593
Satya <satyasairam144@gmail.com>
2024-02-18 11:40:05 +0530

    enabled subnet

--- terraform-test/modules/main.tf
+++ terraform-test/modules/main.tf
@@ -7,15 +7,15 @@ module "vpc" {
   
 }
 
-# module "subnet" {
-#     source = "./subnets"
-#     region = var.region
-#     env = var.env
-#     pub_subnet_cidr = var.pub_subnet_cidr
-#     pvt_subnet_cidr = var.pvt_subnet_cidr
+module "subnet" {
+    source = "./subnets"
+    region = var.region
+    env = var.env
+    pub_subnet_cidr = var.pub_subnet_cidr
+    pvt_subnet_cidr = var.pvt_subnet_cidr
     
   
-# }
+}
 
 output "vpc_id" {
     value = module.vpc.vpc_id
