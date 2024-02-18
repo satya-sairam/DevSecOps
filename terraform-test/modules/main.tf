8904e8fe034fceb619535352f78375c7959ba6a1
Satya <satyasairam144@gmail.com>
2024-02-18 10:58:17 +0530

    outputs changed

--- terraform-test/modules/main.tf
+++ terraform-test/modules/main.tf
@@ -11,18 +11,18 @@ module "subnets" {
 
 
 output "vpc_id" {
-    value = aws_vpc.my_vpc.id
+    value = module.networking.vpc_id
   
 }
 
 
 output "pvt_subnet_id" {
-    value = aws_subnet.pvt_subnet.id
+    value = module.subnets.pvt_subnet_id
   
 }
 
 output "pub_subnet_id" {
-    value = aws_subnet.pub_subnet.id
+    value = module.subnets.pub_subnet_id
   
 }
 
