dde6839c1608df135e8b6cc33fb4cd8e19278581
Satya <satyasairam144@gmail.com>
2024-02-18 10:55:42 +0530

    removed tf backend

--- terraform-test/modules/subnets/subnets.tf
+++ terraform-test/modules/subnets/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "pub_subnet" {
   vpc_id                = data.terraform_remote_state.vpcccc.outputs.vpc_id
   cidr_block            = var.pub_subnet_cidr
   availability_zone     = data.aws_availability_zones.available.names[0]
-  depends_on            = [ aws_vpc.my_vpc ]
+ 
   
 
   tags = {
@@ -15,7 +15,7 @@ resource "aws_subnet" "pvt_subnet" {
   vpc_id                   = data.terraform_remote_state.vpcccc.outputs.vpc_id
   cidr_block               = var.pvt_subnet_cidr
   availability_zone        = data.aws_availability_zones.available.names[1]
-  depends_on              = [ aws_vpc.my_vpc ]
+  
 
   tags = {
     Name = "${var.env}-private-subnet"
